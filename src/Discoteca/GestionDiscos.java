292f47750a32be433167ca087b836e695ca3450b
manuelcespedesrguez-ux <manuelcespedesrguez@gmail.com>
2026-04-10 11:04:06 +0200

    10/04/2026 retoques

--- src/Discoteca/GestionDiscos.java
+++ src/Discoteca/GestionDiscos.java
@@ -1,5 +1,4 @@
 package Discoteca;
-
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
--- src/Inmuebles/Inmueble.java
+++ src/Inmuebles/Inmueble.java
@@ -1,45 +1,27 @@
 package Inmuebles;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class Inmueble {
     
+    public static final Map<String, Double> VALOR_METRO_CUADRADO = new HashMap<>();
+
+    //Insertamos datos en el mapa
+    public static void poblar() {
+        VALOR_METRO_CUADRADO.put("Casa rural", 1500000.0);
+        VALOR_METRO_CUADRADO.put("Casa en conjunto cerrado", 2500000.0);
+        VALOR_METRO_CUADRADO.put("Casa independiente", 3000000.0);
+        VALOR_METRO_CUADRADO.put("Apartaestudio", 1500000.0);
+        VALOR_METRO_CUADRADO.put("Apartamento familiar", 2000000.0);
+        VALOR_METRO_CUADRADO.put("Local comercial", 3000000.0);
+        VALOR_METRO_CUADRADO.put("Oficina", 3500000.0);
+    }
     // Atributos
     protected int id;
     protected int area;
     protected String direccion;
     protected double precioVenta = 0d;
-
-    public int getId() {
-        return id;
-    }
-
-    public void setIdentificadorInmueble(int id) {
-        this.id = id;
-    }
-
-    public int getArea() {
-        return area;
-    }
-
-    public void setArea(int area) {
-        this.area = area;
-    }
-
-    public String getDireccion() {
-        return direccion;
-    }
-
-    public void setDireccion(String direccion) {
-        this.direccion = direccion;
-    }
-
-    public double getPrecioVenta() {
-        return precioVenta;
-    }
-
-    public void setPrecioVenta(double precioVenta) {
-        this.precioVenta = precioVenta;
-    }
-
     
     // Constructor
     public Inmueble(int id, int area, String direccion) {
