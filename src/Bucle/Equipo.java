0e894760b85ced786ba68e34e80f54925c996037
manuelcespedesrguez-ux <manuelcespedesrguez@gmail.com>
2026-04-29 14:43:15 +0200

    29/04/2026 retoques 2.0

---
+++ src/Bucle/Equipo.java
@@ -0,0 +1,37 @@
+package Bucle;
+
+import java.util.ArrayList;
+
+public class Equipo {
+    
+    private String nombre;
+    private String pais;
+    private int tiempo = 0;
+    private int tiempoAcumulado = 0;
+    ArrayList<Ciclista> listaCiclistas;
+
+    public Equipo(String nombre, String pais) {
+        this.nombre = nombre;
+        this.pais = pais;
+        tiempo = 0;
+        listaCiclistas = new ArrayList<Ciclista>();
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getPais() {
+        return pais;
+    }
+
+    public void setPais(String pais) {
+        this.pais = pais;
+    }
+
+
+}
