38fbbe2bdc0b4478e50110c1ceb4eb89f1ae048d
manuelcespedesrguez-ux <manuelcespedesrguez@gmail.com>
2026-05-06 14:48:36 +0200

    06/05/2026

---
+++ src/Competicion/MatchDeportivo.java
@@ -0,0 +1,9 @@
+package Competicion;
+
+public class MatchDeportivo {
+    
+    public void setEquipoLocal(String nombre);
+
+    public void setEquipoVisitante(String nombre);
+
+}
---
+++ src/Competicion/PartidoFutbolLigaEsapnhola.java
@@ -0,0 +1,39 @@
+package Competicion;
+
+public class PartidoFutbolLigaEsapnhola implements PartidoFutbol {
+    
+    private String equipoLocal;
+    private String equipoVisitante;
+    private int golesEquipoLocal;
+    private int golesEquipoVisitante;
+
+    public PartidoFutbolLigaEsapnhola(String equipoLocal, String equipoVisitante, int golesEquipoLocal,
+            int golesEquipoVisitante) {
+        this.equipoLocal = equipoLocal;
+        this.equipoVisitante = equipoVisitante;
+        this.golesEquipoLocal = golesEquipoLocal;
+        this.golesEquipoVisitante = golesEquipoVisitante;
+    }
+
+    @Override
+    public void setGolesEquipoLocal(int numero) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setGolesEquipoLocal'");
+    }
+
+    @Override
+    public void setGolesEquipoVisitante(int numero) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'setGolesEquipoVisitante'");
+    }
+    @Override
+    public String toString() {
+        return "El partido de fútbol de la Liga entre el " + equipoLocal + " y el " + equipoVisitante + " terminó con un resultado de " 
+        + golesEquipoLocal + " a " + golesEquipoVisitante;
+    }
+
+    
+
+    
+
+}
